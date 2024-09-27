a1e4a6d09479d491fdd5ca6c60fbf8b43b43c897
Luc9766 <lucasl678876@gmail.com>
2024-09-27 05:52:11 -0400

    Uploading simulation of data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,19 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Simulates a dataset of TTC Streetcar Delay 
+# Author: Pengyu Li
+# Date: 26 September 2024
+# Contact: pengyu.li@mail.utoronto.ca
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
-
+set.seed(246)
+n<-300
 #### Simulate data ####
-# [...ADD CODE HERE...]
-
-
-
+Date <- sample(as.Date(2024/01/01), as.Date(2024/08/31), n, replace=TRUE)
+Line <- sample(c("301","304","305","306","310","501","503","504","505","506",
+                 "507","508","509","510","511","512"), n, replace=TRUE)
+Time <- sample(c("morning_rush_hours", "evening_rush_hours", "other"), n, replace=TRUE)
+Day <- sample(c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
+                "Saturday", "Sunday"), n, replace=TRUE)
+Bound <- sample(c("W", "E"), n, replace=TRUE)
